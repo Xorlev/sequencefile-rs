567b8febc2130eae548d21895b7006bdbd8e86c7
Michael Rose <elementation@gmail.com>
2016-02-06 04:22:50 -0700

    util: test coverage around vint decoding

--- src/util.rs
+++ src/util.rs
@@ -10,8 +10,10 @@ pub trait ZeroCompress : Read {
         let len = if value >= -112 {
             1
         } else if value < -120 {
+            // [2, 8], negative result
             -119 - value
         } else {
+            // [2, 8], positive result
             -111 - value
         };
 
@@ -19,6 +21,7 @@ pub trait ZeroCompress : Read {
         if len == 1 {
             val = value as i64;
         } else {
+            // shifts buffer to make room for new set-o-bits, ors in the new byte
             for _ in 0..(len - 1) {
                 try!(self.read_exact(&mut raw_buffer));
                 val = val << 8;
@@ -36,3 +39,31 @@ pub trait ZeroCompress : Read {
 
 impl<R> ZeroCompress for R where R: Read
 {}
+
+#[cfg(test)]
+mod tests {
+    use super::ZeroCompress;
+    use std::io::{Read, Cursor};
+
+    #[test]
+    fn decodes_single_byte() {
+        let mut buf = Cursor::new(vec![0b0111_1111]);
+
+        assert_eq!(127, buf.decode_vint64().unwrap());
+    }
+
+    #[test]
+    fn decodes_multi_byte() {
+        let mut buf = Cursor::new(vec![0b1000_1101, 0b1000_0000, 0b0100_0000, 0b0010_1101]);
+
+        assert_eq!(8_405_037, buf.decode_vint64().unwrap());
+    }
+
+
+    #[test]
+    fn decodes_multi_byte_negative() {
+        let mut buf = Cursor::new(vec![0b1000_0101, 0b1000_0000, 0b0100_0000, 0b0010_1101]);
+
+        assert_eq!(-8_405_038, buf.decode_vint64().unwrap());
+    }
+}
